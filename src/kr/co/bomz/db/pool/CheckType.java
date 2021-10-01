b1133940ae1b11b1175d4dacbbb46de78a2c0935
bom bom <bom@bomz.app>
2021-10-01 14:33:48 +0000

    ver 1.0

---
+++ src/kr/co/bomz/db/pool/CheckType.java
@@ -0,0 +1,17 @@
+package kr.co.bomz.db.pool;
+
+/**
+ * 검사 결과
+ * 
+ * @author Bomz
+ * @version 1.0
+ * @since 1.0
+ */
+public enum CheckType {
+	/**		검사 결과 성공		*/
+	SUCCESS,
+	/**		검사 결과 실패		*/
+	FAIL,
+	/**		검사 자격 미달		*/
+	PASS
+}
