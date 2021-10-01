4d55cd184d9ae5f4913563c412e893de22368f53
bom bom <bom@bomz.app>
2021-10-01 14:36:34 +0000

    ver 1.0

---
+++ src/kr/co/bomz/db/pool/StatementType.java
@@ -0,0 +1,14 @@
+package kr.co.bomz.db.pool;
+
+/**
+ * 
+ * @author Bomz
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public enum StatementType {
+	STATEMENT,
+	PREPARED_STATEMENT,
+	CALLABLE_STATEMENT
+}
